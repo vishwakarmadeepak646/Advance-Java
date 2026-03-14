bba26cd1108cf7c0e38c51520b837d9c4db330d9
vishwakarmadeepak646 <vishwakarmadeepak646@gmail.com>
2026-03-14 11:54:47 +0530

    Done;

--- JDBC_Project/src/com/dev/jdbc/MarksheetSelect.java
+++ JDBC_Project/src/com/dev/jdbc/MarksheetSelect.java
@@ -18,13 +18,28 @@ public class MarksheetSelect {
 		
 		ResultSet s = stmt.executeQuery("Select * from Marksheet");
 		
+		System.out.print( "Id");
+		System.out.print("\t" + "Roll No");
+		System.out.print("\t" + "Name");
+		System.out.print("\t\t" + "Physics");
+		System.out.print("\t" + "Chemistry");
+		System.out.print("\t" + "Maths");
+		System.out.print("\t" + "Total");
+		System.out.println("\t" + "Percentage");
+		
 		while(s.next()) {
-			System.out.print("\t" + s.getInt(1));
+			System.out.print(s.getInt(1));
 			System.out.print("\t" + s.getInt(2));
 			System.out.print("\t" + s.getString(3));
 			System.out.print("\t" + s.getInt(4));
 			System.out.print("\t" + s.getInt(5));
-			System.out.println("\t" + s.getInt(6));
+			System.out.print("\t\t" + s.getInt(6));
+			
+			
+			int total = (s.getInt(4)+s.getInt(5)+s.getInt(6));
+			int per = total/3;
+			System.out.print("\t" + total);
+			System.out.println("\t" + per);
 			
 		}
 		
