96ddb37456fb9a45815e9c22ba2947f39061eeaf
vishwakarmadeepak646 <vishwakarmadeepak646@gmail.com>
2026-04-14 23:47:52 +0530

    Cookies

---
+++ Demo-Project/src/main/java/com/rays/cookies/GetCookies.java
@@ -0,0 +1,36 @@
+package com.rays.cookies;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/GetCookies")
+public class GetCookies extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		Cookie[] c = req.getCookies();
+
+		for (int i = 1; i < c.length; i++) {
+
+			Cookie cu = c[i];
+			
+			String name = cu.getName();
+			String value = cu.getValue();
+			
+			System.out.println(name);
+			System.out.println(value);
+
+		}
+		
+		
+
+	}
+
+}
---
+++ Demo-Project/src/main/java/com/rays/cookies/SetCookies.java
@@ -0,0 +1,24 @@
+package com.rays.cookies;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/SetCookies")
+public class SetCookies extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String name = req.getParameter("name");
+		String value = req.getParameter("value");
+		
+		Cookie c = new Cookie(name, value);
+		resp.addCookie(c);
+	}
+
+}
